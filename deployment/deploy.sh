037e5dd90aea8f1edf5b1761222b49dc17656a1f
sam <smuoki@healthcloud.co.ke>
2019-08-01 14:00:39 +0300

    modified permissions of deploy.sh

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -1,8 +1,11 @@
-ssh muokipoll.duckdns.org <<EOF
+#!/bin/sh
+
+ssh muokipolls.duckdns.org <<EOF
   cd muoki
   git pull
   source venv/bin/activate
   pip install -r /home/sam/muoki/pollsapp/requirements.txt
+  cd /home/sam/muoki/pollsapp/
   ./manage.py migrate
   # sudo supervisorctl restart djtrump
   exit
