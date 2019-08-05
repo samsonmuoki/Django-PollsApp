8a9dc233a53a2f3aa20cdb2d0cced99728c3d672
sam <smuoki@healthcloud.co.ke>
2019-08-05 18:53:05 +0300

    added new tests

--- pollsapp/polls/views.py
+++ pollsapp/polls/views.py
@@ -29,9 +29,9 @@ def index(request):
     return render(request, 'polls/index.html', context)
 
 
-# def detail(request, question_id):
-#     question = get_object_or_404(Question, pk=question_id)
-#     return render(request, 'polls/detail.html', {'question': question})
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
 
 
 def results(request, question_id):
