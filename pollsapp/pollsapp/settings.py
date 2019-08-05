435cbfa1cdc70c4cb9b7202036c73bd93df7868d
sam <smuoki@healthcloud.co.ke>
2019-08-05 11:59:56 +0300

    modified config.yml

--- pollsapp/pollsapp/settings.py
+++ pollsapp/pollsapp/settings.py
@@ -113,15 +113,15 @@ DATABASES = {
     }
 }
 
-CACHES = {
-    'default': {
-        'BACKEND': 'django_redis.cache.RedisCache',
-        'LOCATION': 'redis://127.0.0.1:6379/',
-        'OPTIONS': {
-            'CLIENT_CLASS': 'django_redis.client.DefaultClient',
-        }
-    }
-}
+# CACHES = {
+#     'default': {
+#         'BACKEND': 'django_redis.cache.RedisCache',
+#         'LOCATION': 'redis://127.0.0.1:6379/',
+#         'OPTIONS': {
+#             'CLIENT_CLASS': 'django_redis.client.DefaultClient',
+#         }
+#     }
+# }
 
 
 # Password validation
