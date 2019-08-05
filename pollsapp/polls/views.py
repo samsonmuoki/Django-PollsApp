19fce919b52e733b06761629d0e91fc00612dbf6
sam <smuoki@healthcloud.co.ke>
2019-08-05 18:48:58 +0300

    added new tests

--- pollsapp/polls/views.py
+++ pollsapp/polls/views.py
@@ -30,8 +30,8 @@ def index(request):
 
 
 def detail(request, question_id):
-    # question = get_object_or_404(Question, pk=question_id)
-    # return render(request, 'polls/detail.html', {'question': question})
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
 
 
 def results(request, question_id):
@@ -51,8 +51,7 @@ def vote(request, question_id):
     else:
         selected_choice.votes += 1
         selected_choice.save()
-        return HttpResponseRedirect(reverse('polls:results', args=(question.id,
-            )))
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
 
 
 class IndexView(generic.ListView):
